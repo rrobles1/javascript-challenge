095c06d292937d53271623a0dee47b2380c298a1
Roberto Robles <robertorobles@Robertos-MacBook-Air.local>
2019-10-31 18:34:32 -0500

    third commit

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -2,3 +2,11 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+// Use D3 to select the table
+const table = d3.select("table");
+table.attr("class", "table table-striped");
+let tbody = d3.select("tbody");
+console.log(data);
+data.forEach(function(ufoSightings) {
+    console.log(ufoSightings);
+  });
